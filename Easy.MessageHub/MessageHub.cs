adb1cb4fbcf50a6aedb76a30f7052dce2a2fa539
Stephen Burrows <stephen.burrows@outlook.com>
2019-04-16 23:21:53 +0100

    Autofac working
     Class MessageHub contractor was private thus autofac could not create an instance set to public

--- Easy.MessageHub/MessageHub.cs
+++ Easy.MessageHub/MessageHub.cs
@@ -2,33 +2,31 @@
 {
     using System;
     using System.Diagnostics;
+
 #if NET_STANDARD
     using System.Reflection;
-#endif   
+#endif
 
-    /// <summary>
-    /// An implementation of the <c>Event Aggregator</c> pattern.
-    /// </summary>
+    /// <summary> An implementation of the <c> Event Aggregator </c> pattern. </summary>
     public sealed class MessageHub : IMessageHub
     {
         private readonly Subscriptions _subscriptions;
         private Action<Type, object> _globalHandler;
         private Action<Guid, Exception> _globalErrorHandler;
 
-        private MessageHub() => _subscriptions = new Subscriptions();
+        public MessageHub() => _subscriptions = new Subscriptions();
 
-        /// <summary>
-        /// Returns a single instance of the <see cref="MessageHub"/>
-        /// </summary>
+        /// <summary> Returns a single instance of the <see cref="MessageHub" /> </summary>
         public static MessageHub Instance { get; } = new MessageHub();
 
-        /// <summary>
-        /// Registers a callback which is invoked on every message published by the <see cref="MessageHub"/>.
-        /// <remarks>Invoking this method with a new <paramref name="onMessage"/>overwrites the previous one.</remarks>
+        /// <summary> 
+        /// Registers a callback which is invoked on every message published by the
+        /// <see cref="MessageHub" />. <remarks> Invoking this method with a new
+        /// <paramref name="onMessage" /> overwrites the previous one. </remarks>
         /// </summary>
-        /// <param name="onMessage">
-        /// The callback to invoke on every message
-        /// <remarks>The callback receives the type of the message and the message as arguments</remarks>
+        /// <param name="onMessage"> 
+        /// The callback to invoke on every message <remarks> The callback receives the type of the
+        /// message and the message as arguments </remarks>
         /// </param>
         public void RegisterGlobalHandler(Action<Type, object> onMessage)
         {
@@ -36,9 +34,9 @@
             _globalHandler = onMessage;
         }
 
-        /// <summary>
-        /// Invoked if an error occurs when publishing a message to a subscriber.
-        /// <remarks>Invoking this method with a new <paramref name="onError"/>overwrites the previous one.</remarks>
+        /// <summary> 
+        /// Invoked if an error occurs when publishing a message to a subscriber. <remarks> Invoking
+        /// this method with a new <paramref name="onError" /> overwrites the previous one. </remarks>
         /// </summary>
         public void RegisterGlobalErrorHandler(Action<Guid, Exception> onError)
         {
@@ -46,10 +44,8 @@
             _globalErrorHandler = onError;
         }
 
-        /// <summary>
-        /// Publishes the <paramref name="message"/> on the <see cref="MessageHub"/>.
-        /// </summary>
-        /// <param name="message">The message to published</param>
+        /// <summary> Publishes the <paramref name="message" /> on the <see cref="MessageHub" />. </summary>
+        /// <param name="message"> The message to published </param>
         public void Publish<T>(T message)
         {
             var localSubscriptions = _subscriptions.GetTheLatestSubscriptions();
@@ -82,49 +78,49 @@
             }
         }
 
-        /// <summary>
-        /// Subscribes a callback against the <see cref="MessageHub"/> for a specific type of message.
+        /// <summary> 
+        /// Subscribes a callback against the <see cref="MessageHub" /> for a specific type of message.
         /// </summary>
-        /// <typeparam name="T">The type of message to subscribe to</typeparam>
-        /// <param name="action">The callback to be invoked once the message is published on the <see cref="MessageHub"/></param>
-        /// <returns>The token representing the subscription</returns>
+        /// <typeparam name="T"> The type of message to subscribe to </typeparam>
+        /// <param name="action"> 
+        /// The callback to be invoked once the message is published on the <see cref="MessageHub" />
+        /// </param>
+        /// <returns> The token representing the subscription </returns>
         public Guid Subscribe<T>(Action<T> action) => Subscribe(action, TimeSpan.Zero);
 
-        /// <summary>
-        /// Subscribes a callback against the <see cref="MessageHub"/> for a specific type of message.
+        /// <summary> 
+        /// Subscribes a callback against the <see cref="MessageHub" /> for a specific type of message.
         /// </summary>
-        /// <typeparam name="T">The type of message to subscribe to</typeparam>
-        /// <param name="action">The callback to be invoked once the message is published on the <see cref="MessageHub"/></param>
-        /// <param name="throttleBy">The <see cref="TimeSpan"/> specifying the rate at which subscription is throttled</param>
-        /// <returns>The token representing the subscription</returns>
+        /// <typeparam name="T"> The type of message to subscribe to </typeparam>
+        /// <param name="action"> 
+        /// The callback to be invoked once the message is published on the <see cref="MessageHub" />
+        /// </param>
+        /// <param name="throttleBy"> 
+        /// The <see cref="TimeSpan" /> specifying the rate at which subscription is throttled
+        /// </param>
+        /// <returns> The token representing the subscription </returns>
         public Guid Subscribe<T>(Action<T> action, TimeSpan throttleBy)
         {
             EnsureNotNull(action);
             return _subscriptions.Register(throttleBy, action);
         }
 
-        /// <summary>
-        /// Unsubscribes a subscription from the <see cref="MessageHub"/>.
-        /// </summary>
-        /// <param name="token">The token representing the subscription</param>
+        /// <summary> Unsubscribes a subscription from the <see cref="MessageHub" />. </summary>
+        /// <param name="token"> The token representing the subscription </param>
         public void Unsubscribe(Guid token) => _subscriptions.UnRegister(token);
 
-        /// <summary>
-        /// Checks if a specific subscription is active on the <see cref="MessageHub"/>.
-        /// </summary>
-        /// <param name="token">The token representing the subscription</param>
-        /// <returns><c>True</c> if the subscription is active otherwise <c>False</c></returns>
+        /// <summary> Checks if a specific subscription is active on the <see cref="MessageHub" />. </summary>
+        /// <param name="token"> The token representing the subscription </param>
+        /// <returns> <c> True </c> if the subscription is active otherwise <c> False </c> </returns>
         public bool IsSubscribed(Guid token) => _subscriptions.IsRegistered(token);
 
-        /// <summary>
-        /// Clears all the subscriptions from the <see cref="MessageHub"/>.
-        /// <remarks>The global handler and the global error handler are not affected</remarks>
+        /// <summary> 
+        /// Clears all the subscriptions from the <see cref="MessageHub" />. <remarks> The global
+        /// handler and the global error handler are not affected </remarks>
         /// </summary>
         public void ClearSubscriptions() => _subscriptions.Clear(false);
 
-        /// <summary>
-        /// Disposes the <see cref="MessageHub"/>.
-        /// </summary>
+        /// <summary> Disposes the <see cref="MessageHub" />. </summary>
         public void Dispose()
         {
             _globalHandler = null;
