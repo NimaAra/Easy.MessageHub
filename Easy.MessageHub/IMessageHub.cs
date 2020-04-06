1ab8211b61ab79d7bbd753dcc5340df9090292b4
Rune Kristiansen Kvamme <RUK@equinor.com>
2020-04-06 18:23:04 +0200

    Added new subscribe method to interface.

--- Easy.MessageHub/IMessageHub.cs
+++ Easy.MessageHub/IMessageHub.cs
@@ -48,6 +48,14 @@
         /// Subscribes a callback against the <see cref="MessageHub"/> for a specific type of message.
         /// </summary>
         /// <typeparam name="T">The type of message to subscribe to</typeparam>
+        /// <param name="func">The async callback to be invoked once the message is published on the <see cref="MessageHub"/></param>
+        /// <returns>The token representing the subscription</returns>
+        Guid Subscribe<T>(Func<T, Task> func);
+
+        /// <summary>
+        /// Subscribes a callback against the <see cref="MessageHub"/> for a specific type of message.
+        /// </summary>
+        /// <typeparam name="T">The type of message to subscribe to</typeparam>
         /// <param name="action">The callback to be invoked once the message is published on the <see cref="MessageHub"/></param>
         /// <param name="throttleBy">The <see cref="TimeSpan"/> specifying the rate at which subscription is throttled</param>
         /// <returns>The token representing the subscription</returns>
